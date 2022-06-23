70ef9f753cfc45e336a7ce6e50df1ce210847b27
anandasatriaadi <putu.satriaadi@gmail.com>
2022-06-23 12:42:54 +0700

    feat: update product api

--- src/main/java/com/binaracademy/secondhand/service/ProductServiceImpl.java
+++ src/main/java/com/binaracademy/secondhand/service/ProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.binaracademy.secondhand.service;
 
 import java.io.File;
+import java.net.URI;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -69,6 +71,87 @@ public class ProductServiceImpl implements ProductService {
             product.setCategoryId(categoryExist.get().getId());
             product.setCategory(categoryExist.get());
             Product productDb = productRepository.save(product);
+
+            // ======== Upload Image and Add Image to ProductImage  ========
+            MultipartFile[] images = uploadProductDto.getImages();
+            for (int i = 0; i < images.length; i++) {
+                try {
+                    File convertFile = new File(System.getProperty("java.io.tmpdir") + "/" + images[i].getOriginalFilename());
+                    images[i].transferTo(convertFile);
+                    String imageUrl = (String) SecondhandApplication.cloudinary.uploader().upload(convertFile, ObjectUtils.emptyMap()).get("url");
+                    
+                    ProductImage productImage = new ProductImage();
+                    productImage.setImageUrl(imageUrl);
+                    productImage.setProductId(productDb.getId());
+                    productImageRepository.save(productImage);
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                }
+            }
+
+            return productDb;
+        } else {
+            String msg = "Product ";
+            msg += uploadProductDto.getName() == null ? "name, " : "";
+            msg += uploadProductDto.getDescription() == null ? "description, " : "";
+            msg += uploadProductDto.getCategoryId() == null ? "category, " : "";
+            msg += uploadProductDto.getPrice() == null ? "price, " : "";
+            msg += uploadProductDto.getAddress() == null ? "address " : "";
+            msg += "can't be null";
+            throw new IllegalArgumentException(msg);
+        }
+    }
+
+    @Override
+    public Product updateProduct(String username, Long id, UploadProductDto uploadProductDto) {
+        // ======== Check Repository ========
+        if(!productRepository.findById(id).isPresent()) {
+            throw new IllegalArgumentException("Product not found");
+        }
+
+        // ======== Check Images Count ========
+        if(uploadProductDto.getImages().length > 4) {
+            throw new IllegalArgumentException("Max 4 images");
+        }
+        
+        // ======== Check if category exists ========
+        Optional<Category> categoryExist = categoryRepository.findById(uploadProductDto.getCategoryId());
+        if(!categoryExist.isPresent()) {
+            throw new IllegalArgumentException("Category not found");
+        }
+        
+        // ======== Check if there are any nulls ========
+        if(uploadProductDto.getName() != null && uploadProductDto.getDescription() != null && uploadProductDto.getCategoryId() != null &&
+            uploadProductDto.getPrice() != null && uploadProductDto.getAddress() != null) {
+            
+            Long userId = userRepository.findByUsername(username).getId();
+            
+            // ======== Assign DTO to Model ========
+            Product product = new Product();
+            product.setId(id);
+            product.setName(uploadProductDto.getName());
+            product.setDescription(uploadProductDto.getDescription());
+            product.setPrice(uploadProductDto.getPrice());
+            product.setAddress(uploadProductDto.getAddress());
+            product.setUserId(userId);
+            product.setCategoryId(categoryExist.get().getId());
+            product.setCategory(categoryExist.get());
+            Product productDb = productRepository.save(product);
+
+            // ======== Delete old images before upload ========
+            List<ProductImage> oldImages = productRepository.findAllProductImages(id);
+            for (ProductImage oldImage : oldImages) {
+                try {
+                    String publicId = Paths.get(new URI(oldImage.getImageUrl()).getPath()).getFileName().toString();
+                    publicId = publicId.substring(0, publicId.lastIndexOf("."));
+
+                    SecondhandApplication.cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap()).toString();
+
+                    productImageRepository.delete(oldImage);
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                }
+            }
 
             // ======== Upload Image and Add Image to ProductImage  ========
             MultipartFile[] images = uploadProductDto.getImages();
