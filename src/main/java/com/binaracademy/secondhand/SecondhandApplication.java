766f40753e557e9883dfabfd835081c37d127729
anandasatriaadi <putu.satriaadi@gmail.com>
2022-06-28 16:28:13 +0800

    feat: add check user and user response dto

--- src/main/java/com/binaracademy/secondhand/SecondhandApplication.java
+++ src/main/java/com/binaracademy/secondhand/SecondhandApplication.java
@@ -1,12 +1,19 @@
 package com.binaracademy.secondhand;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SecondhandApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecondhandApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
