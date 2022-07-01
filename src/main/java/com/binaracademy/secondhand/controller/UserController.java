187e54c2c5db003a64f55f2c877346b68cbd3b72
anandasatriaadi <putu.satriaadi@gmail.com>
2022-07-01 15:39:09 +0800

    feat: add refresh token api

--- src/main/java/com/binaracademy/secondhand/controller/UserController.java
+++ src/main/java/com/binaracademy/secondhand/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.binaracademy.secondhand.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.binaracademy.secondhand.dto.ResponseUserDto;
 import com.binaracademy.secondhand.dto.RestDto;
 import com.binaracademy.secondhand.dto.UploadUserDto;
+import com.binaracademy.secondhand.model.User;
 import com.binaracademy.secondhand.repository.UserRepository;
 import com.binaracademy.secondhand.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,15 +30,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Slf4j
 public class UserController {
 
     private final UserRepository userRepository;
     private final UserService userService;
+    private final ModelMapper modelMapper;
 
     // ========================================================================
     //   Get all users while not on production
@@ -34,6 +52,58 @@ public class UserController {
             return ResponseEntity.ok(new RestDto(200, "ok", userService.getAllUsers()));
         }
         return null;
+    }
+
+    // ========================================================================
+    //   Get New Access Token from Refresh token
+    // ========================================================================
+    @GetMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Refresh Token is missing");
+        } else {
+            try {
+                String secretCode = System.getenv("jwtsecret") == null ? "th3r34ls3cr3t1sn0wh3r3t0b3f0und" : System.getenv("jwtsecret");
+
+                String token = authHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256(secretCode.getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(token);
+
+                String username = decodedJWT.getSubject();
+                User user = modelMapper.map(userService.getUser(username), User.class);
+
+                // ======== Generate Access Token ========
+                String accessToken = JWT
+                    .create()
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                    .withIssuer(request.getRequestURL().toString())
+                    .sign(algorithm);
+
+                // ======== Generate Refresh Token ========
+                String refreshToken = JWT
+                    .create()
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 6 * 60 * 60 * 1000))
+                    .withIssuer(request.getRequestURL().toString())
+                    .sign(algorithm);
+
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("username", user.getUsername());
+                tokens.put("accessToken", accessToken);
+                tokens.put("refreshToken", refreshToken);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), new RestDto(200, "ok", tokens));
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper()
+                    .writeValue(response.getOutputStream(), new RestDto(HttpServletResponse.SC_UNAUTHORIZED, "Token expired", ""));
+                log.error("Error processing refresh token");
+            }
+        }
     }
 
     // ========================================================================
