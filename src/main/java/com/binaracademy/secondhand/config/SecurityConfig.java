43c9fd2869ee7a439ec79708dad70359a179479f
anandasatriaadi <putu.satriaadi@gmail.com>
2022-07-07 14:40:32 +0800

    refactor: reorganize code

--- src/main/java/com/binaracademy/secondhand/config/SecurityConfig.java
+++ src/main/java/com/binaracademy/secondhand/config/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
