c8a89e10b14370c8c669409ce98d78dd82988432
anandasatriaadi <putu.satriaadi@gmail.com>
2022-07-12 15:04:14 +0800

    feat: add offer implementation

---
+++ src/main/java/com/binaracademy/secondhand/controller/OfferController.java
@@ -0,0 +1,10 @@
+package com.binaracademy.secondhand.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class OfferController {}
---
+++ src/main/java/com/binaracademy/secondhand/util/enums/OfferStatus.java
@@ -0,0 +1,7 @@
+package com.binaracademy.secondhand.util.enums;
+
+public enum OfferStatus {
+    WAITING,
+    ACCEPTED,
+    DECLINED,
+}
