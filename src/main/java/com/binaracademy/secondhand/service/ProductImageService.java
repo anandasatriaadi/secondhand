aa04634ab162604d5001993082f984314fd2e483
anandasatriaadi <putu.satriaadi@gmail.com>
2022-07-23 19:28:45 +0800

    feat: add thumbnail url to product

--- src/main/java/com/binaracademy/secondhand/service/ProductImageService.java
+++ src/main/java/com/binaracademy/secondhand/service/ProductImageService.java
@@ -5,7 +5,8 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ProductImageService {
-    List<ProductImage> getProductImage(Long productId);
+    List<ProductImage> getProductImageList(Long productId);
+    ProductImage getProductImage(Long productId);
     boolean saveProductImages(Long productId, MultipartFile[] images);
     boolean deleteProductImages(List<ProductImage> images);
 }
